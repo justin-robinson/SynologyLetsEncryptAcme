9c83aa2642601775f579e03906c1a3b3bd352baf
Justin Robinson <jurobn@amazon.com>
2020-10-30 23:57:48 -0700

    update to new syno certificate structuree

--- index.js
+++ index.js
@@ -13,21 +13,71 @@ const handler = (path) => {
         domain: JSON.parse(fs.readFileSync(join(path, dir.name, 'renew.json'))).domains
       };
     })
-    .forEach(entry => {
-      console.log(`Updating ${entry.domain} in directory ${entry.dirName}`);
-      const acme = spawnSync('./acme.sh', [
+    .forEach(({domain, dirName}) => {
+      console.log(`Updating ${domain} in directory ${dirName}`);
+      const reverseProxyFolder = findReverseProxyFolder(domain);
+      const command = ('./acme.sh');
+      const issueArgs = createCertArgs(join(path, dirName));
+      const installArgs = createCertArgs(reverseProxyFolder);
+      const reloadCommands = [
+        `achme.sh --install-cert -d ${domain} ${installArgs.join(' ')}`,
+        'sudo synoservice --restart nginx',
+        'sudo synoservice --restart nmbd',
+        'sudo synoservice --restart avahi'
+      ];
+      const commandArgs = [
         '--issue',
-        '-d', entry.domain,
+        '-d', domain,
         '--dns', 'dns_aws',
-        '--certpath', join(path, entry.dirName, 'cert.pem'),
-        '--keypath', join(path, entry.dirName, 'privkey.pem'),
-        '--fullchainpath', join(path, entry.dirName, 'fullchain.pem'),
-        '--capath', join(path, entry.dirName, 'chain.pem'),
-        '--reloadcmd', 'sudo synoservice --restart nginx; sudo synoservice --restart nmbd; sudo synoservice --restart avahi'
-      ]);
+        ...issueArgs,
+        '--reloadcmd', reloadCommands.join('; ')
+      ];
+      console.log(`running command ${command} ${commandArgs.join(' ')}`);
+      const acme = spawnSync(command, commandArgs);
       console.log( `stderr: ${acme.stderr}` );
       console.log( `stdout: ${acme.stdout}` );
     });
+}
+
+const findReverseProxyFolder = (domainToFind) => {
+  const path = '/usr/syno/etc/certificate/ReverseProxy';
+  const folders = fs.readdirSync(path, {withFileTypes: true})
+    .filter(file => file.isDirectory())
+    .map(dir => {
+      const command = 'openssl';
+      const args = [
+        'x509',
+        '-in', `${join(path, dir.name, 'cert.pem')}`,
+        '-text',
+        '-noout',
+      ];
+      const certText = spawnSync(command, args);
+      const domainName = certText.stdout.toString()
+                                        .split(/\n/)
+                                        .filter(line => line.trim().startsWith('DNS:'))
+                                        .map(dnsLine => dnsLine.split(':')[1]);
+      return {
+        dirName: dir.name,
+        domain: domainName.length ? domainName[0] : null
+      }
+    })
+    .filter(({domain}) => {
+      return domain === domainToFind;
+    })
+    .map(({dirName}) => {
+      return dirName;
+    });
+
+  return folders.length ? join(path, folders[0]) : "";
+}
+
+const createCertArgs = (folder) => {
+  return [
+    '--certpath', join(folder, 'cert.pem'),
+    '--keypath', join(folder, 'privkey.pem'),
+    '--fullchainpath', join(folder, 'fullchain.pem'),
+    '--capath', join(folder, 'chain.pem')
+  ]
 }
 
 handler('/usr/syno/etc/certificate/_archive');
